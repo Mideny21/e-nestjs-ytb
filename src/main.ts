da8106386570b583015f275929a1b2b6d2832a18
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2025-07-03 10:33:47 +0300

    desc changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DelayInterceptor } from './common/interceptors/delay-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform:true, transformOptions:{
-    enableImplicitConversion: true
-  } }));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, transform: true, transformOptions: {
+      enableImplicitConversion: true
+    }
+  }));
+
+  app.useGlobalInterceptors(new DelayInterceptor(2000));
   await app.listen(3000);
 }
 bootstrap();
